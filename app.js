2d42fc5784f4fc97464d4963659b4cc1b558d77d
MacarenaAbregu <103336385+MacarenaAbregu@users.noreply.github.com>
2022-07-14 09:53:30 -0300

    clase 14/15

--- app.js
+++ app.js
@@ -141,3 +141,35 @@ btn.addEventListener("click", () => {
   });
 }); 
 
+
+
+const lista = document.querySelector("#listado");
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((producto) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+                <h4>${producto.nombre}</h4>
+                <p>${producto.precio}</p>
+                <p>Código: ${producto.id}</p>
+                <hr/>
+            `;
+      lista.append(li);
+    });
+  }); 
+
+const pedirDatos = async () => {
+  const respuesta = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await respuesta.json();
+  data.forEach((post) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h4>${post.title}</h4>
+        <p>${post.body}</p>
+    `;
+    lista.append(li);
+  });
+};
+pedirDatos();
